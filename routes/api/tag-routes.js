dba8bf7056571485abfc3a5ed3593d35771cdcfb
Deiontre10 <travontazlowry@gmail.com>
2023-01-17 14:50:15 -0500

    updated tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,77 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
+  try {
+    const data = await Tag.findAll({
+      include: [{
+        model: Product,
+        // through: ProductTag
+
+      }]
+    }
+    );
+    res.status(200).json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  // find one tag by its `id` value
+  try {
+    const data = await Tag.findByPk(req.params.id, {
+      include: [{
+        model: Product,
+
+      }]
+    }
+    );
+    res.status(200).json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const data = await Tag.create(req.body);
+    res.status(200).json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  // update a tag by its `id` value
+  try {
+    const data = await Tag.update(req.body, {
+      where: { id: req.params.id }
+    });
+    res.status(200).json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  // delete a tag by its `id` value
+  try {
+    const data = await Tag.destroy({
+      where: { id: req.params.id }
+    });
+    res.status(200).json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
